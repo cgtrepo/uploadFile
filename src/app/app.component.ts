cb244625fa9e1ed1df1af4f7f2f4e9331d76e389
cgtrepo <112641868+cgtrepo@users.noreply.github.com>
2023-05-31 22:34:43 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { CrudService } from './service/crud.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'uploadFile';
+  image: File;
+  mutipleImage = []
+
+  constructor(private http: HttpClient, private crudService: CrudService) {
+
+  }
+
+
+  selectImage(event: any) {
+    if(event.target.files.length > 0) {
+      const file = event.target.files[0]
+      this.image = file
+      console.log(this.image)
+
+    }
+  }
+
+  onChange(){
+    const formatData = new FormData()
+
+    formatData.append('file', this.image)
+
+    this.crudService.postFiles(this.image).subscribe({
+      next: (res) => {
+          console.log(res)
+      }
+    })
+  }
 }
